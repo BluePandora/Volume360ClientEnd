04fc2444daea4048cbf1e648db4707678a5a96a1
BluePandora <bluepandora666@gmail.com>
2015-04-07 00:29:00 +0600

    log in screen created and scene updated

--- src/main/java/com/project/volume360/screen/LogInScreen.java
+++ src/main/java/com/project/volume360/screen/LogInScreen.java
@@ -1,8 +1,24 @@
 package com.project.volume360.screen;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class LogInScreen extends Screen {
+
+	@FXML
+	private TextField userNameTextField;
+	@FXML
+	private TextField passwordTextField;
+	@FXML
+	private Button signInButton;
 
 	public LogInScreen() {
 	}
 
+	@FXML
+	private void handleSignIn(ActionEvent actionEvent) {
+
+	}
 }
