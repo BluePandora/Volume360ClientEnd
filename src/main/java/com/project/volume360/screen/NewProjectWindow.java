13d29668f61bddd6a890d394fe6e694c2c89c014
RaggedyCoder <bluepandora666@gmail.com>
2015-05-26 20:47:58 +0600

    added the filechooser option

--- src/main/java/com/project/volume360/screen/NewProjectWindow.java
+++ src/main/java/com/project/volume360/screen/NewProjectWindow.java
@@ -1,5 +1,12 @@
 package com.project.volume360.screen;
 
+import java.io.File;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
 import com.project.volume360.application.item.NewProject;
 import com.project.volume360.screen.annotation.FXMLLocation;
 
@@ -7,6 +14,9 @@ import com.project.volume360.screen.annotation.FXMLLocation;
 public class NewProjectWindow extends Screen {
 
 	private AddNewProjectListener addNewProjectListener;
+
+	@FXML
+	private Label workOrderLabel;
 
 	public NewProjectWindow() {
 		// TODO Auto-generated constructor stub
@@ -30,5 +40,19 @@ public class NewProjectWindow extends Screen {
 
 	public interface AddNewProjectListener {
 		public void addNewProject(NewProject newProject);
+	}
+
+	@FXML
+	private void fileChooser() {
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Work Order");
+		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter(
+				"image files (*.jpg,*.jpeg,*.png)", "*.jpg", "*.jpeg", "*.png");
+		fileChooser.getExtensionFilters().add(extFilter);
+		File file = fileChooser.showOpenDialog(new Stage());
+		if (file != null) {
+			workOrderLabel.setText(file.getName());
+		}
+
 	}
 }
