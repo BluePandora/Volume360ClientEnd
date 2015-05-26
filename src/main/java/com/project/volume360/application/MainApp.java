8158d2322a5f1e36bb5e2ad055eecadc726f6d7c
RaggedyCoder <bluepandora666@gmail.com>
2015-05-27 00:44:51 +0600

    commit

--- src/main/java/com/project/volume360/application/MainApp.java
+++ src/main/java/com/project/volume360/application/MainApp.java
@@ -29,6 +29,7 @@ public class MainApp extends Application implements ApplicationListener {
 	public static void main(String[] args) {
 		launch(args);
 	}
+	
 
 	@Override
 	public void changeScene(int tag) {
